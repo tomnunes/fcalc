01d23718def941dea9da64eff67adb7d143b0936
Alberto <albertokaneto@gmail.com>
2016-10-03 21:31:36 -0300

    Created new function is_positive_number and changed calculate_poupa

--- fcalc.js
+++ fcalc.js
@@ -1,5 +1,11 @@
-function calculate_poupa(value, tax, time) {
-  for (var i = 0; i < time; i++) {
+function calculate_poupa(value, tax, month) {
+  var rendimento;
+
+  if (!is_positive_number(value) && !is_positive_number(tax) && !is_positive_number(month)) {
+  	alert('Parameters should be a positive number!');
+  }
+
+  for (var i = 0; i < month; i++) {
     rendimento = value * tax;
     value = value + rendimento;
   }
@@ -8,4 +14,12 @@ function calculate_poupa(value, tax, time) {
 
 function round(a) {
   return Math.round(a*100)/100;
+}
+
+function is_positive_number(value) {
+  if (!isNaN(value) && value > 0) {
+  	return true;
+  } else {
+  	return false;
+  }
 }
--- tests.js
+++ tests.js
@@ -1,4 +1,24 @@
-QUnit.test( "calculate_poupa should pass", function( assert ) {
+QUnit.test( "calculate_poupa should return the expected result", function( assert ) {
   var result = calculate_poupa(100,0.0065,3);
   assert.ok( result == 101.96, "Passed!" );
+});
+
+QUnit.test( "calculate_poupa should return an alert message", function( assert ) {
+  var result = calculate_poupa('','','');
+  assert.ok( result == false, "Passed!" );
+});
+
+QUnit.test( "is_positive_number should return false with a string", function( assert ) {
+  var result = is_positive_number('A');
+  assert.ok( result === false, "Passed!" );
+});
+
+QUnit.test( "is_positive_number should return false with a negative number", function( assert ) {
+  var result = is_positive_number(-1);
+  assert.ok( result === false, "Passed!" );
+});
+
+QUnit.test( "is_positive_number should return true with a positive number", function( assert ) {
+  var result = is_positive_number(100);
+  assert.ok( result === true, "Passed!" );
 });
